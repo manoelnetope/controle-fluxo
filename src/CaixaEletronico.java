ecefb1a74a7748fc5e2e04fb233c2881addfad01
Manoel Marques <manoelneto_pe@hotmail.com>
2024-11-13 19:46:00 -0300

    Caixa-Eletronico

---
+++ src/CaixaEletronico.java
@@ -0,0 +1,11 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 450000.0;
+       double valorSolicitado = 34500.0;
+
+       if(valorSolicitado < saldo)
+        saldo = saldo - valorSolicitado;
+
+        System.out.println("Manoel Marques seu saldo é: " + "$"+saldo);
+    }
+}
